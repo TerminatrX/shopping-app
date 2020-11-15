6c77c844a78299c7662eeafa05bafae3c307d540
DESKTOP-0BAS9DR\Varun <varun2273@gmail.com>
2020-11-15 11:15:18 -0600

    Total Cost Calculation Test File created

---
+++ src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculationTest.java
@@ -0,0 +1,17 @@
+/* Varun Nair
+    SE 333
+    Final Project
+ */
+
+package edu.depaul.se433.shoppingapp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+// In this file there will be Strong Normal Tests, Weak Robust Tests and Boundary Tests
+public class TotalCostCalculationTest {
+
+
+}
