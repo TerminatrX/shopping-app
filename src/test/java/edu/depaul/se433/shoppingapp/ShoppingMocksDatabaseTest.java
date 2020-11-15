ee114bd215d54e7c7c96abc4c707c0c85863cfe9
DESKTOP-0BAS9DR\Varun <varun2273@gmail.com>
2020-11-15 13:20:45 -0600

    Mocks test file has been created with all necessary imports

---
+++ src/test/java/edu/depaul/se433/shoppingapp/ShoppingMocksDatabaseTest.java
@@ -0,0 +1,12 @@
+package edu.depaul.se433.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ShoppingMocksDatabaseTest {
+
+}
