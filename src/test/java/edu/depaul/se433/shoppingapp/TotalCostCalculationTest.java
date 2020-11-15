14b59926efffef880aca0ccd2bb3ea3ed74acb67
DESKTOP-0BAS9DR\Varun <varun2273@gmail.com>
2020-11-15 11:34:52 -0600

    Updated syntatical errors in the strong normal tests for total cost calculation

--- src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculationTest.java
+++ src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculationTest.java
@@ -36,20 +36,46 @@ public class TotalCostCalculationTest {
     @Test
     @DisplayName("Cart Below $50 w/No Tax w/Next Day Shipping")
     void below50NoTaxNextDay(){
-        double cost = TotalCostCalculator.calculate(10.00, "WA", ShippingType.STANDARD);
+        double cost = TotalCostCalculator.calculate(10.00, "WA", ShippingType.NEXT_DAY);
         assertEquals(35.00, cost);
     }
 
     @Test
     @DisplayName("Cart Above $50 w/No Tax w/Next Day Shipping")
     void above50NoTaxNextDay(){
-        double cost = TotalCostCalculator.calculate(60.00, "WA", ShippingType.STANDARD);
+        double cost = TotalCostCalculator.calculate(60.00, "WA", ShippingType.NEXT_DAY);
         assertEquals(85.00, cost);
     }
 
     //Tax w/ Standard Shipping
+    @Test
+    @DisplayName("Cart Below $50 w/Tax w/Standard Shipping")
+    void below50TaxStandard(){
+        double cost = TotalCostCalculator.calculate(10.00, "IL", ShippingType.STANDARD);
+        assertEquals(15.00, cost);
+    }
+
+    @Test
+    @DisplayName("Cart Above $50 w/Tax w/Standard Shipping")
+    void above50TaxStandard() {
+        double cost = TotalCostCalculator.calculate(60.00, "IL", ShippingType.STANDARD);
+        assertEquals(60.00, cost);
+    }
 
     //Tax w/Next Day Shipping (+$25)
+        @Test
+        @DisplayName("Cart Below $50 w/Tax w/Next Day Shipping")
+        void below50TaxNextDay(){
+            double cost = TotalCostCalculator.calculate(10.00, "IL", ShippingType.NEXT_DAY);
+            assertEquals(35.00, cost);
+        }
+
+        @Test
+        @DisplayName("Cart Above $50 w/Tax w/Next Day Shipping")
+        void above50TaxNextDay(){
+            double cost = TotalCostCalculator.calculate(60.00, "IL", ShippingType.NEXT_DAY);
+            assertEquals(85.00, cost);
+        }
 
     //Weak Robust Tests
 
