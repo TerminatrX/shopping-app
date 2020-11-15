658b84b08411e6a2dd50d8d565bed092b54dcac8
DESKTOP-0BAS9DR\Varun <varun2273@gmail.com>
2020-11-15 11:31:46 -0600

    New Tests added for Total Cost Calculation

--- src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculationTest.java
+++ src/test/java/edu/depaul/se433/shoppingapp/TotalCostCalculationTest.java
@@ -15,6 +15,42 @@ public class TotalCostCalculationTest {
 
     //Strong Normal Tests
 
+
+    //No Tax w/ Standard Shipping
+    @Test
+    @DisplayName("Cart Below $50 w/No Tax w/Standard Shipping")
+    void below50NoTaxStandard(){
+        double cost = TotalCostCalculator.calculate(10.00, "WA", ShippingType.STANDARD);
+        assertEquals(15.00, cost);
+    }
+
+    @Test
+    @DisplayName("Cart Above $50 w/No Tax w/Standard Shipping")
+    void above50NoTaxStandard(){
+        double cost = TotalCostCalculator.calculate(60.00, "WA", ShippingType.STANDARD);
+        assertEquals(60.00, cost);
+    }
+
+    //No Tax w/Next Day Shipping (+$25)
+
+    @Test
+    @DisplayName("Cart Below $50 w/No Tax w/Next Day Shipping")
+    void below50NoTaxNextDay(){
+        double cost = TotalCostCalculator.calculate(10.00, "WA", ShippingType.STANDARD);
+        assertEquals(35.00, cost);
+    }
+
+    @Test
+    @DisplayName("Cart Above $50 w/No Tax w/Next Day Shipping")
+    void above50NoTaxNextDay(){
+        double cost = TotalCostCalculator.calculate(60.00, "WA", ShippingType.STANDARD);
+        assertEquals(85.00, cost);
+    }
+
+    //Tax w/ Standard Shipping
+
+    //Tax w/Next Day Shipping (+$25)
+
     //Weak Robust Tests
 
     //Boundary Tests
